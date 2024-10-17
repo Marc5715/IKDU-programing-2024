0d059709994e78f575c4782d7a0b2a84bc3c1fb5
Marc5715 <marcus@juulnielsen.dk>
2024-10-17 16:12:10 +0200

    Update AgeScript.cs

--- IKDU Programming 2024/Assets/Scripts/AgeScript.cs
+++ IKDU Programming 2024/Assets/Scripts/AgeScript.cs
@@ -7,15 +7,17 @@ public class AgeScript : MonoBehaviour
     float awesomeness = 96.4f;
     string myFirstName = "Marcus";
     int currentAge = 23;
-    int ageIncrease =1;
+    int ageInAYear = 1;
     public int timeTravel;
 
     // Start is called before the first frame update
     void Start()
     {
-        Debug.LogFormat("Hello my name is {0} my current age is: {1}. next year I'll be: {2} My awesomeness detector says that my awesomeness today is = {3}%", myFirstName, currentAge, (currentAge + ageIncrease), awesomeness) ;
+        ///
+        Debug.LogFormat("Hello my name is {0} my current age is: {1}. next year I'll be: {2} My awesomeness detector says that my awesomeness today is = {3}%", 
+        myFirstName, currentAge, AgeIncrease(currentAge, ageInAYear), awesomeness) ;
 
-        Debug.LogFormat("In {0} years I'll be: {1}", timeTravel, (currentAge + timeTravel));
+        Debug.LogFormat("In {0} years {1} will be: {2}", timeTravel, myFirstName, AgeIncrease(currentAge, timeTravel));
     }
 
     // Update is called once per frame
@@ -23,4 +25,12 @@ public class AgeScript : MonoBehaviour
     {
         
     }
+    /// <summary>
+    /// Adds together two ints and returns the result.
+    int AgeIncrease (int a, int b)
+    {
+        a += b;
+        return (a);
+    }
+    
 }
